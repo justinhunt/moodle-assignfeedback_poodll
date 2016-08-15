97b690b0e57d4350bc7497d89809dbc11ee7d1b4
Justin Hunt <poodllsupport@gmail.com>
2016-08-15 04:22:26 -0400

    support for poodll 3

--- version.php
+++ version.php
@@ -22,11 +22,10 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015121101;
-$plugin->requires  = 2015051100;
-$plugin->component = 'assignfeedback_poodll';
+$plugin->version   = 2016081401;
+$plugin->requires  = 2016052300;//moodle 3.1.0
+$plugin->component =  'assignfeedback_poodll';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.6 (Build 2015121101)';
-$plugin->dependencies = array('filter_poodll' => 2015121101);
+$plugin->release   = '3.0.0 (Build 2016081401)';
+$plugin->dependencies = array('filter_poodll' => 2016081401);
